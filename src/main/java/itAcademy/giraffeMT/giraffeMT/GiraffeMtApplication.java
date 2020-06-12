6edf8416bb08a4c2ab3c1a8bb5711b8252b67fe6
Nuriza <nuriza.samidinovna@gmail.com>
2020-06-12 06:12:37 +0600

    project initialization

---
+++ src/main/java/itAcademy/giraffeMT/giraffeMT/GiraffeMtApplication.java
@@ -0,0 +1,13 @@
+package itAcademy.giraffeMT.giraffeMT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraffeMtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiraffeMtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itAcademy/giraffeMT/giraffeMT/GiraffeMtApplicationTests.java
@@ -0,0 +1,13 @@
+package itAcademy.giraffeMT.giraffeMT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraffeMtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
